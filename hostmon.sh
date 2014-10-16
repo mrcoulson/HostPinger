152960e557fb438253d0d9f2a92bbd48a59b57b0
Jeremy Coulson <jeremycoulson@gmail.com>
2014-10-16 16:26:08 -0400

    Separated email and status into different functions. Also updated path to config file.

--- hostmon.sh
+++ hostmon.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
 # Load recipients and hosts from config file.
-source hostmonconfig.cfg
+source ./hostmonconfig.cfg
 
 # Get now.
 now="$(date)"
 
-# Iterate through hosts.
-for i in "${hosts[@]}"
-do
-	# Add timestamp.
-	status="$now"
-	# Add host.
-	status="$status \t $i"
-	# Create count and set to results of ping piped to grep piped to line count.
-	count=$(ping -c 1 $i | grep icmp* | wc -l)
-	# If there are no lines (no returned pings)...
-	if [[ $count -eq 0 ]]
-	then
-		status="$status \t Down"
-		echo "$i is down. Please advise." | mail -s "$i is down!" $recipients
-	else
-		status="$status \t Alive"
-	fi
-	# Echo concatenated status to txt file.
-	echo -e $status >> hostmonlog.txt
-done
+function getStatus {
+	# Iterate through hosts.
+	for i in "${hosts[@]}"
+	do
+		# Add timestamp.
+		status="$now"
+		# Add host.
+		status="$status \t $i"
+		# Create count and set to results of ping piped to grep piped to line count.
+		count=$(ping -c 1 $i | grep icmp* | wc -l)
+		# If there are no lines (no returned pings)...
+		if [[ $count -eq 0 ]]
+		then
+			status="$status \t Down"
+			sendEmail
+		else
+			status="$status \t Alive"
+		fi
+		# Echo concatenated status txt to file.
+		echo -e $status >> hostmonresult-tabs.txt
+	done
+}
+
+function sendEmail {
+	echo "$i is down at $now. Please advise." | mail -s "$i is down!" $recipients
+}
+
+getStatus
